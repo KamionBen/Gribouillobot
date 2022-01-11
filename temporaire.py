0cb394fdfbcf60ea527421093dc6dd955ae92bd3
KamionBen <kamionben@gmail.com>
2022-01-11 16:03:14 +0100

    Changement entre 'main' et 'temporaire'

--- temporaire.py
+++ temporaire.py
@@ -1,97 +1,69 @@
 import csv
-from random import choice, shuffle
 
-VERSION = "0.1.2"
+VERSION = "0.1.1"
+
+""" Cette version ne fait rien d'autre qu'afficher le lexique """
+""" Pas en convaincu par ce système, peut-être utiliser une classe pour le lexique ? """
 
 
-class Gribouillobot:
-    def __init__(self, fichier):
-        """ Charge le fichier donné et classe les mots dans un dict selon les catégories """
-        """ Chaque catégorie a une liste "_ignore" associée pour stocker les mots utilisés récemment """
-        """ Thèmes """
-        # Tous : techniques, cultures, espace_naturel, espace_humain, temps_naturel, temps_humain, climat, histoire
-        # Thèmes art : techniques, cultures
-        # Thèmes humains : cultures, espace_humain, temps_humain, histoire
-        # Thèmes naturels : espace_naturel, temps_naturel, climat
-        """ Mots """
-        # Tous : objets_tendus, objets_détendus, états_tendus, états_détendus, animaux_tendus, animaux_détendus
-        # Mots tendus : objets_tendus, états_tendus, animaux_tendus
-        # Mots détendus : objets_détendus, états_détendus, animaux_détendus
-        # Objets : objets_tendus, objets_détendus
-        # Etats : états_tendus, états_détendus
-        # Animaux : animaux_tendus, animaux_détendus
+def importer_lexique(fichier):
+    """ Charge le fichier .csv et renvoie un dictionnaire"""
+    with open(fichier, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        semaine = None
+        lexique = {}
+        for row in reader:
+            title = row[0]
+            if title == 'semaine':
+                semaine = row[1]
+            elif title[0] == "#":
+                pass
+            else:
+                lexique[title] = sorted(row[1:])
+    return semaine, lexique
 
-        self.fichier = fichier  # Pour que la fonction importer_lexique() fonctionne
-        self.semaine, self.lexique = self.importer_lexique()  # Par commodité, la variable semaine est stockée avec le lexique
 
-    def importer_lexique(self):
-        """ Charge le fichier .csv et renvoie un dictionnaire"""
-        with open(self.fichier, newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            semaine = None
-            lexique = {}
-            for row in reader:
-                title = row[0]
-                if title == 'semaine':
-                    semaine = int(row[1])
-                elif title[0] == "#":
-                    pass
-                else:
-                    lexique[title] = sorted(row[1:])
-        return semaine, lexique
+def sauvegarder_lexique(fichier, sort=True):
+    """ Sauvegarde le lexique et trie les listes par ordre alphabétiques (ou pas) """
+    # TODO
 
-    def afficher_lexique(self):
-        """ Affiche toutes les listes du lexique dans la console """
-        for key, value in self.lexique.items():
-            print(f"{key}:    {value}")
 
-    def sauvegarder_lexique(self):
-        """ Fais les opérations de fin et sauvegarde le lexique """
-        """ Opérations : semaine += 1, réhabiliter les mots qu'il faut, trier les listes """
-        """ Penser à rajouter les '#' pour la lisibilité """
-        pass
+def choisir_theme(lexique, categorie=None):
+    """ Renvoie un thème au hasard, si aucune catégorie n'est donnée, choisit au hasard """
+    # Le thème choisi est retiré de la liste principale et ajouté à la liste "_ignore" correspondante
+    # TODO
 
-    def choisir_theme(self, categorie=None):
-        """ Choisis un thème dans la catégorie donnée """
-        """ Si aucune catégorie n'est donnée, continue le cycle Arts -> Naturel -> Humains """
-        cycle = ['Arts', 'Naturel', 'Humains']
-        if categorie is None:
-            categorie = cycle[self.semaine % len(cycle)]
 
-        choix = None
-        if categorie == 'Arts':
-            choix = choice(('techniques', 'cultures'))
-        elif categorie == 'Naturel':
-            choix = choice(('espace_naturel', 'temps_naturel', 'climat'))
-        elif categorie == 'Humains':
-            choix = choice(('cultures', 'espace_humain', 'temps_humain', 'histoire'))
-        else:
-            raise TypeError(f"La catégorie entrée, '{categorie}', n'est pas valide")
+def choisir_mots(lexique, nombre=6, categorie=None):
+    """ Renvoie une liste de six mots, si aucune catégorie n'est donnée, choisit au hasard """
+    # Les mots choisis sont retirés de leur listes principales respective
+    # et ajoutés aux listes "_ignore" correspondantes
+    # TODO
 
-        shuffle(self.lexique[choix])  # On mélange le paquet de mots
-        selection = self.lexique[choix].pop(0)  # On retire le premier
-        self.lexique[f"{choix}_ignore"].append(selection)  # On ajoute le mot à la liste des mots ignorés
 
-        return selection
+def rehabiliter_theme(lexique, temps=8):
+    """ Vérifie les listes "_ignore", et remets les thèmes dans la liste principale
+    s'ils sont ignorés depuis plus longtemps que la variable 'temps' (8 semaines par défaut) """
+    # ATTENTION : Pour le décompte, il faudra sûrement ajouter une None à chaque liste inutilisée
+    # Todo
 
-    def choisir_mots(self, compte=6, sauf=None, seulement=None):
-        """ Choisis N mots au hasard dans les catégories autorisées """
+
+def rehabiliter_mots(lexique, temps=8):
+    """ Vérifie les listes "_ignore", et remets les mots dans la liste principale
+    s'ils sont ignorés depuis plus longtemps que la variable 'temps' (8 semaines par défaut) """
+    # ATTENTION : Pour le décompte, il faudra sûrement ajouter une None à chaque liste inutilisée
+    # Todo
+
+
+def print_lexique(lexique):
+    for key, value in lexique.items():
+        print(f"{key}:    {value}")
 
 
 if __name__ == '__main__':
-    """ Cette partie de code se trouverait plutôt dans un fichier interface.py """
-    bot = Gribouillobot('lexique.csv')  # Initialisation du bot
-    print(f"-- Gribouillobot --\n - Version {VERSION} -\n\nSemaine {bot.semaine}\n")
+    semaine, lexique = importer_lexique('lexique.csv')
 
-    print("Sélection d'un thème au hasard :")
-    print(f"{bot.choisir_theme().capitalize()}")
+    print(f"Gribouillobot v{VERSION}\n-- Semaine n°{semaine}\n")
+    print_lexique(lexique)
 
-    print("\nAffichage du lexique :")
-    bot.afficher_lexique()  # On voit que le thème choisi apparaît bien dans la liste "_ignore"
-
-    print("\nTestons la sécurité :")
-    print(f"{bot.choisir_theme('Humains').capitalize()}")
-    print('Okay')
-    print(f"{bot.choisir_theme(0).capitalize()}")
-    print("Si ça s'écrit, la sécurité ne marche pas")
 
